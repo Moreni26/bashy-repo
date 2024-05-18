e3cd3d1be6064023a20da0cc8d24fa40f95b8f7d
moreni26 <ayomide2605@gmail.com>
2024-05-18 11:55:06 +0100

    this is oop repo

---
+++ javascript-oop/bank.js
@@ -0,0 +1,114 @@
+class Customer {
+    constructor(firstName, lastName, address, phoneNumber) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this._address = address;
+        this._phoneNumber = phoneNumber;
+    }
+    set firstName(firstName) {
+        this._firstName = firstName;
+    }
+    get firstName() {
+        return this._firstName;
+    }
+    set lastName(lastName) {
+        this._lastName = lastName;
+    }
+    get lastName() {
+        return this._lastName;
+    }
+    set address(address) {
+        this._address = address;
+    }
+    get address() {
+        return this._address;
+    }
+    set phoneNumber(phoneNumber) {
+        this._phoneNumber = phoneNumber;
+    }
+    get phoneNumber() {
+        return this._phoneNumber;
+    }
+}
+
+class Account {
+    constructor(owner, active = true, balance = 0.00) {
+        this._owner = owner;
+        this._active = active;
+        this._balance = balance;
+    }
+    set accountNumber(number) {
+        this._accountNumber = number;
+    }
+    get accountNumber() {
+        return this._accountNumber;
+    }
+    set owner(name) {
+        this._owner = name;
+    }
+    get owner() {
+        return this._owner;
+    }
+    set active(yes) {
+        this._active = yes;
+    }
+    get active() {
+        return this._active;
+    }
+    set balance(number) {
+        this._balance = number;
+    }
+    get balance() {
+        return this._balance;
+    }
+}
+
+class Bank {
+    constructor(name, address) {
+        this.name = name;
+        this.address = address;
+        this.customers = {};  
+        this.accounts = {};  
+    }
+
+    registerCustomer(firstName, lastName, address, phoneNumber) {
+        if (!(phoneNumber in this.customers)) {
+            this.customers[phoneNumber] = new Customer(firstName, lastName, address, phoneNumber);
+        }
+    }
+
+    createAccount(customer) {
+        if (!(customer.phoneNumber in this.accounts)) {
+            let account = new Account(customer);
+            this.accounts[customer.phoneNumber] = account;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    debitAccount(account, amount) {
+        if (account.balance >= amount) {
+            account.balance -= amount;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    creditAccount(account, amount) {
+        account.balance += amount;
+        return true;
+    }
+
+    giveLoan(account, amount) {
+        return this.creditAccount(account, amount);  
+    }
+}
+
+let bank = new Bank("Skye Bank", "123 Main St");
+bank.registerCustomer("Basirat", "Usman", "Main Street", "08166666");
+let customer = bank.customers["08166666"];
+let newAccountCreated = bank.createAccount(customer); 
+console.log(bank);
+
